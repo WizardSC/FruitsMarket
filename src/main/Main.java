c72522dc6e11ebf0abe65daf1b185b6e498ae5e7
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-06-19 22:13:16 +0700

    right menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,12 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../View/market.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
